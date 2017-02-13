9146df40338f6b4e1e6c57028be1d85c0b39ffbc
PabloDons <omar.saad0705@gmail.com>
2017-02-13 22:43:43 +0100

    fancified poll display

--- discord_bot.js
+++ discord_bot.js
@@ -1,3 +1,4 @@
+/* jshint esversion:6*/
 var fs = require('fs');
 
 try {
@@ -52,7 +53,7 @@ Permissions.checkPermission = function (user,permission){
 		return allowed;
 	} catch(e){}
 	return false;
-}
+};
 fs.writeFile("./permissions.json",JSON.stringify(Permissions,null,2));
 
 //load config data
@@ -84,71 +85,71 @@ try{
 }
 
 var commands = {
-	"alias": {
-		usage: "<name> <actual command>",
-		description: "Creates command aliases. Useful for making simple commands on the fly",
-		process: function(bot,msg,suffix) {
-			var args = suffix.split(" ");
-			var name = args.shift();
-			if(!name){
-				msg.channel.sendMessage(Config.commandPrefix + "alias " + this.usage + "\n" + this.description);
-			} else if(commands[name] || name === "help"){
-				msg.channel.sendMessage("overwriting commands with aliases is not allowed!");
-			} else {
-				var command = args.shift();
-				aliases[name] = [command, args.join(" ")];
-				//now save the new alias
-				require("fs").writeFile("./alias.json",JSON.stringify(aliases,null,2), null);
-				msg.channel.sendMessage("created alias " + name);
-			}
-		}
-	},
-	"aliases": {
-		description: "lists all recorded aliases",
+	// "alias": {
+	// 	usage: "<name> <actual command>",
+	// 	description: "Creates command aliases. Useful for making simple commands on the fly",
+	// 	process: function(bot,msg,suffix) {
+	// 		var args = suffix.split(" ");
+	// 		var name = args.shift();
+	// 		if(!name){
+	// 			msg.channel.sendMessage(Config.commandPrefix + "alias " + this.usage + "\n" + this.description);
+	// 		} else if(commands[name] || name === "help"){
+	// 			msg.channel.sendMessage("overwriting commands with aliases is not allowed!");
+	// 		} else {
+	// 			var command = args.shift();
+	// 			aliases[name] = [command, args.join(" ")];
+	// 			//now save the new alias
+	// 			require("fs").writeFile("./alias.json",JSON.stringify(aliases,null,2), null);
+	// 			msg.channel.sendMessage("created alias " + name);
+	// 		}
+	// 	}
+	// },
+	// "aliases": {
+	// 	description: "lists all recorded aliases",
+	// 	process: function(bot, msg, suffix) {
+	// 		var text = "current aliases:\n";
+	// 		for(var a in aliases){
+	// 			if(typeof a === 'string')
+	// 				text += a + " ";
+	// 		}
+	// 		msg.channel.sendMessage(text);
+	// 	}
+	// },
+	"ping": {
+		description: "responds pong, useful for checking if bot is alive",
 		process: function(bot, msg, suffix) {
-			var text = "current aliases:\n";
-			for(var a in aliases){
-				if(typeof a === 'string')
-					text += a + " ";
+			msg.channel.sendMessage( msg.author+" pong!");
+			if(suffix){
+				msg.channel.sendMessage( "note that !ping takes no arguments!");
 			}
-			msg.channel.sendMessage(text);
 		}
 	},
-    "ping": {
-        description: "responds pong, useful for checking if bot is alive",
-        process: function(bot, msg, suffix) {
-            msg.channel.sendMessage( msg.author+" pong!");
-            if(suffix){
-                msg.channel.sendMessage( "note that !ping takes no arguments!");
-            }
-        }
-    },
-    "idle": {
-				usage: "[status]",
-        description: "sets bot status to idle",
-        process: function(bot,msg,suffix){
-	    bot.user.setStatus("idle");
-	    bot.user.setGame(suffix);
-	}
-    },
-    "online": {
-				usage: "[status]",
-        description: "sets bot status to online",
-        process: function(bot,msg,suffix){
-	    bot.user.setStatus("online");
-	    bot.user.setGame(suffix);
-	}
-    },
-    "say": {
-        usage: "<message>",
-        description: "bot says message",
-        process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix);}
-    },
-	"announce": {
-        usage: "<message>",
-        description: "bot says message with text to speech",
-        process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix,{tts:true});}
-    },
+	// "idle": {
+	// 	usage: "[status]",
+	//     description: "sets bot status to idle",
+	//     process: function(bot,msg,suffix){
+	//     	bot.user.setStatus("idle");
+	//     	bot.user.setGame(suffix);
+	// 	}
+	// },
+	// "online": {
+	// 			usage: "[status]",
+	//     description: "sets bot status to online",
+	//     process: function(bot,msg,suffix){
+	//     	bot.user.setStatus("online");
+	//     	bot.user.setGame(suffix);
+	// 	}
+	// },
+	"say": {
+		usage: "<message>",
+		description: "bot says message",
+		process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix);}
+	},
+	// "announce": {
+	//     usage: "<message>",
+	//     description: "bot says message with text to speech",
+	//     process: function(bot,msg,suffix){ msg.channel.sendMessage(suffix,{tts:true});}
+	// },
 	"msg": {
 		usage: "<user> <message to leave user>",
 		description: "leaves a message for a user the next time they come online",
@@ -168,7 +169,7 @@ var commands = {
 				content: target + ", " + msg.author + " said: " + message
 			};
 			updateMessagebox();
-			msg.channel.sendMessage("message saved.")
+			msg.channel.sendMessage("message saved.");
 		}
 	},
 	"eval": {
@@ -176,7 +177,7 @@ var commands = {
 		description: 'Executes arbitrary javascript in the bot process. User must have "eval" permission',
 		process: function(bot,msg,suffix) {
 			if(Permissions.checkPermission(msg.author,"eval")){
-				msg.channel.sendMessage( eval(suffix,bot));
+				msg.channel.sendMessage( eval(suffix, bot));
 			} else {
 				msg.channel.sendMessage( msg.author + " doesn't have permission to execute eval!");
 			}
@@ -185,12 +186,12 @@ var commands = {
 };
 
 if(AuthDetails.hasOwnProperty("client_id")){
-	commands["invite"] = {
+	commands.invite = {
 		description: "generates an invite link you can use to invite the bot to your server",
 		process: function(bot,msg,suffix){
 			msg.channel.sendMessage("invite link: https://discordapp.com/oauth2/authorize?&client_id=" + AuthDetails.client_id + "&scope=bot&permissions=470019135");
 		}
-	}
+	};
 }
 
 
@@ -223,10 +224,10 @@ bot.on("disconnected", function () {
 function checkMessageForCommand(msg, isEdit) {
 	//check if message is a command
 	if(msg.author.id != bot.user.id && (msg.content.startsWith(Config.commandPrefix))){
-        console.log("treating " + msg.content + " from " + msg.author + " as command");
+		console.log("treating " + msg.content + " from " + msg.author + " as command");
 		var cmdTxt = msg.content.split(" ")[0].substring(Config.commandPrefix.length);
-        var suffix = msg.content.substring(cmdTxt.length+Config.commandPrefix.length+1);//add one for the ! and one for the space
-        if(msg.isMentioned(bot.user)){
+		var suffix = msg.content.substring(cmdTxt.length+Config.commandPrefix.length+1);//add one for the ! and one for the space
+		if(msg.isMentioned(bot.user)){
 			try {
 				cmdTxt = msg.content.split(" ")[1];
 				suffix = msg.content.substring(bot.user.mention().length+cmdTxt.length+Config.commandPrefix.length+1);
@@ -234,7 +235,7 @@ function checkMessageForCommand(msg, isEdit) {
 				msg.channel.sendMessage("Yes?");
 				return;
 			}
-        }
+		}
 		alias = aliases[cmdTxt];
 		if(alias){
 			console.log(cmdTxt + " is an alias, constructed command is " + alias.join(" ") + " " + suffix);
@@ -242,60 +243,60 @@ function checkMessageForCommand(msg, isEdit) {
 			suffix = alias[1] + " " + suffix;
 		}
 		var cmd = commands[cmdTxt];
-        if(cmdTxt === "help"){
-            //help is special since it iterates over the other commands
-						if(suffix){
-							var cmds = suffix.split(" ").filter(function(cmd){return commands[cmd]});
-							var info = "";
-							for(var i=0;i<cmds.length;i++) {
-								var cmd = cmds[i];
-								info += "**"+Config.commandPrefix + cmd+"**";
-								var usage = commands[cmd].usage;
-								if(usage){
-									info += " " + usage;
-								}
-								var description = commands[cmd].description;
-								if(description instanceof Function){
-									description = description();
-								}
-								if(description){
-									info += "\n\t" + description;
-								}
-								info += "\n"
-							}
-							msg.channel.sendMessage(info);
-						} else {
-							msg.author.sendMessage("**Available Commands:**").then(function(){
-								var batch = "";
-								var sortedCommands = Object.keys(commands).sort();
-								for(var i in sortedCommands) {
-									var cmd = sortedCommands[i];
-									var info = "**"+Config.commandPrefix + cmd+"**";
-									var usage = commands[cmd].usage;
-									if(usage){
-										info += " " + usage;
-									}
-									var description = commands[cmd].description;
-									if(description instanceof Function){
-										description = description();
-									}
-									if(description){
-										info += "\n\t" + description;
-									}
-									var newBatch = batch + "\n" + info;
-									if(newBatch.length > (1024 - 8)){ //limit message length
-										msg.author.sendMessage(batch);
-										batch = info;
-									} else {
-										batch = newBatch
-									}
-								}
-								if(batch.length > 0){
-									msg.author.sendMessage(batch);
-								}
-						});
+		if(cmdTxt === "help"){
+			//help is special since it iterates over the other commands
+			if(suffix){
+				var cmds = suffix.split(" ").filter(function(cmd){return commands[cmd];});
+				var info = "";
+				for(var i=0;i<cmds.length;i++) {
+					cmd = cmds[i];
+					info += "**"+Config.commandPrefix + cmd+"**";
+					var usage = commands[cmd].usage;
+					if(usage){
+						info += " " + usage;
 					}
-        }
+					var description = commands[cmd].description;
+					if(description instanceof Function){
+						description = description();
+					}
+					if(description){
+						info += "\n\t" + description;
+					}
+					info += "\n";
+				}
+				msg.channel.sendMessage(info);
+			} else {
+				msg.author.sendMessage("**Available Commands:**").then(function(){
+					var batch = "";
+					var sortedCommands = Object.keys(commands).sort();
+					for(var i in sortedCommands) {
+						var cmd = sortedCommands[i];
+						var info = "**"+Config.commandPrefix + cmd+"**";
+						var usage = commands[cmd].usage;
+						if(usage){
+							info += " " + usage;
+						}
+						var description = commands[cmd].description;
+						if(description instanceof Function){
+							description = description();
+						}
+						if(description){
+							info += "\n\t" + description;
+						}
+						var newBatch = batch + "\n" + info;
+						if(newBatch.length > (1024 - 8)){ //limit message length
+							msg.author.sendMessage(batch);
+							batch = info;
+						} else {
+							batch = newBatch;
+						}
+					}
+					if(batch.length > 0){
+						msg.author.sendMessage(batch);
+					}
+				});
+			}
+		}
 		else if(cmd) {
 			if(Permissions.checkPermission(msg.author,cmdTxt)){
 				try{
@@ -303,7 +304,7 @@ function checkMessageForCommand(msg, isEdit) {
 				} catch(e){
 					var msgTxt = "command " + cmdTxt + " failed :(";
 					if(Config.debug){
-						 msgTxt += "\n" + e.stack;
+						msgTxt += "\n" + e.stack;
 					}
 					msg.channel.sendMessage(msgTxt);
 				}
@@ -311,21 +312,21 @@ function checkMessageForCommand(msg, isEdit) {
 				msg.channel.sendMessage("You are not allowed to run " + cmdTxt + "!");
 			}
 		} else {
-			msg.channel.sendMessage(cmdTxt + " not recognized as a command!").then((message => message.delete(5000)))
+			msg.channel.sendMessage(cmdTxt + " not recognized as a command!").then((message => message.delete(5000)));
 		}
 	} else {
 		//message isn't a command or is from us
-        //drop our own messages to prevent feedback loops
-        if(msg.author == bot.user){
-            return;
-        }
+		//drop our own messages to prevent feedback loops
+		if(msg.author == bot.user){
+			return;
+		}
 
-        if (msg.author != bot.user && msg.isMentioned(bot.user)) {
-                msg.channel.sendMessage(msg.author + ", you called?");
-        } else {
+		if (msg.author != bot.user && msg.isMentioned(bot.user)) {
+			msg.channel.sendMessage(msg.author + ", you called?");
+		} else {
 
-				}
-    }
+		}
+	}
 }
 
 bot.on("message", (msg) => checkMessageForCommand(msg, false));
@@ -340,33 +341,37 @@ bot.on("presence", function(user,status,gameId) {
 	console.log(user+" went "+status);
 	//}
 	try{
-	if(status != 'offline'){
-		if(messagebox.hasOwnProperty(user.id)){
-			console.log("found message for " + user.id);
-			var message = messagebox[user.id];
-			var channel = bot.channels.get("id",message.channel);
-			delete messagebox[user.id];
-			updateMessagebox();
-			bot.sendMessage(channel,message.content);
+		if(status != 'offline'){
+			if(messagebox.hasOwnProperty(user.id)){
+				console.log("found message for " + user.id);
+				var message = messagebox[user.id];
+				var channel = bot.channels.get("id",message.channel);
+				delete messagebox[user.id];
+				updateMessagebox();
+				bot.sendMessage(channel,message.content);
+			}
 		}
-	}
 	}catch(e){}
 });
 
 
 exports.addCommand = function(commandName, commandObject){
-    try {
-        commands[commandName] = commandObject;
-    } catch(err){
-        console.log(err);
-    }
-}
+	try {
+		commands[commandName] = commandObject;
+	} catch(err){
+		console.log(err);
+	}
+};
 exports.commandCount = function(){
-    return Object.keys(commands).length;
-}
+	return Object.keys(commands).length;
+};
 if(AuthDetails.bot_token){
 	console.log("logging in with token");
 	bot.login(AuthDetails.bot_token);
 } else {
 	console.log("Logging in with user credentials is no longer supported!\nYou can use token based log in with a user account, see\nhttps://discord.js.org/#/docs/main/master/general/updating");
 }
+
+process.on('SIGINT', function() {
+	bot.destroy().then(process.exit());
+});
--- plugins/Polling/polling.js
+++ plugins/Polling/polling.js
@@ -1,13 +1,29 @@
+var polling = {};
+
 function showTally(tally) {
-	var content = tally.name;
-	content+="\nvote using: poll vote <option>";
+	var fields = [];
 	for (var i in tally.tally) {
-		content += "\n"+i+": "+tally.tally[i];
+		fields.push({name:i, value:tally.tally[i].toString()});
 	}
 
-	return content;
+	return {
+		description:"to vote, do: poll vote <option>",
+		color:0x00ff00,
+		timestamp: new Date(),
+		// footer: {
+		// 	text:"this is the fotter",
+		// 	icon_url:"https://i.imgur.com/qEpGpTc.png"
+		// },
+		thumbnail: {
+			url:"http://pngimg.com/upload/snowflakes_PNG7545.png"
+		},
+		author: {
+			name:tally.name
+		},
+		fields: fields
+	};
 }
-
+var pollRole = "PollMaster";
 exports.commands = [
     "poll"
 ];
@@ -20,32 +36,46 @@ exports.poll = {
         var pollingIn = msg.guild;
         if (!pollingIn) {
             // privmsg
+			msg.channel.sendMessage("You cannot start a message in a private message");
             return;
         }
-        console.log(args);
         if (args[0] == "vote") {
+			console.log("1")
             if (args.length < 2) {
                 msg.channel.sendMessage("usage: poll vote <option>");
             }
+			console.log("2")
             if (polling[pollingIn.id].tally.hasOwnProperty(args[1])) {
-                if (args[1] == "<@"+msg.author.id+">") {
+				console.log("3")
+                if (RegExp("\\<\\@\\!?"+msg.author.id+"\\>").test(args[1])) {
+					console.log("6")
                     msg.channel.sendMessage("You can't vote for yourself!");
                 } else {
+					console.log("7")
                     if (polling[pollingIn.id].votes.hasOwnProperty(msg.author.id)) {
                         polling[pollingIn.id].tally[polling[pollingIn.id].votes[msg.author.id]]-=1;
                     }
                     polling[pollingIn.id].votes[msg.author.id] = args[1];
                     polling[pollingIn.id].tally[args[1]]+=1;
-                    polling[pollingIn.id].display.edit(showTally(polling[pollingIn.id]));
+                    polling[pollingIn.id].display.edit("",{embed:showTally(polling[pollingIn.id])});
                     msg.channel.sendMessage("You've successfully voted!");
-
                 }
+				console.log("4")
             }
+			console.log("5")
         } else if (args[0] == "end") {
+			if (!msg.guild.roles.find('name', pollRole).members.get(msg.author.id)) {
+				msg.channel.sendMessage("You don't have permission to do this!");
+				return;
+			}
             polling[pollingIn.id].status = false;
             msg.channel.sendMessage("Poll ended!");
-            polling[pollingIn.id].display.edit("POLL HAS ENDED!\n"+showTally(polling[pollingIn.id]));
+            polling[pollingIn.id].display.edit("POLL HAS ENDED!",{embed:showTally(polling[pollingIn.id])});
         } else if (args[0] == "start") {
+			if (!msg.guild.roles.find('name', pollRole).members.get(msg.author.id)) {
+				msg.channel.sendMessage("You don't have permission to do this!");
+				return;
+			}
             if (args.length < 4) {
                 msg.channel.sendMessage("usage: poll start <poll_name> <option1> <option2> [option3 [option4 [...]]]");
                 return;
@@ -67,8 +97,7 @@ exports.poll = {
                     return out;
                 })(),
             };
-            msg.channel.sendMessage(showTally(polling[pollingIn.id])).then(function(a){polling[pollingIn.id].display = a;});
-
+            msg.channel.sendEmbed(showTally(polling[pollingIn.id])).then(function(a){polling[pollingIn.id].display = a;});
         }
     }
-}
+};
